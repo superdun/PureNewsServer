0b3e45ca6b1a6774a0f7f5efdc64039dafdc510a
superdun <475098936@qq.com>
2019-03-28 19:08:04 +0800

    'fix'

--- app/views/Api.py
+++ app/views/Api.py
@@ -183,7 +183,7 @@ def WeInfo():
     else:
         averageCount = 0
     list = flus
-    greaterCount = Customer.query.filter(Customer.count>=total_count).count()-1
+    greaterCount = Customer.query.filter(Customer.count>total_count).count()
     allCount  = Customer.query.count()-1
     defeat = int(greaterCount*10000.0/allCount)/100
     markers = [{"latitude":p.lat,"longitude":p.lon,"iconPath":p.img,"id":p.id,"name":"","width":30,"height":30}  for p in totalIllPeople.all()]
